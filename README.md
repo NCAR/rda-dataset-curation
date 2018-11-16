7aa019a7e19f3d20d3c96431dfc9a2905fb4769b
Riley Conroy <rpconroy@ucar.edu>
2018-11-16 11:51:38 -0700

    Added some basic documentation

--- README.md
+++
@@ -1,2 +0,0 @@
-# rda-dataset-curation
-Provides generic tools for data curation as well as dataset specific curation software 
--- test/test_collateByDimension.py
+++ test/test_collateByDimension.py
@@ -3,6 +3,16 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) +"/../")
 import common.collateByDimension as cbd
 import create_test_nc_file as create_nc
+from subprocess import call
+
+name = create_nc.create()
+name2 = create_nc.create()
 
 
-cbd.collate(, ['test1.nc','test2.nc'], varname=None, output_filename="out.nc" ):
+cbd.collate(, ['test1.nc','test2.nc'], varname=None, output_filename="out.nc" )
+
+
+call(['rm', name])
+call(['rm', name2])
+
+
