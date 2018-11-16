c76283699852cf8e8a6a3b7e312f26f64b1523f7
Riley Conroy <rpconroy@ucar.edu>
2018-11-16 10:57:04 -0700

    Added create test file program

--- test/test_collateByDimension.py
+++ test/test_collateByDimension.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 import sys, os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) +"/../")
-import common.collateByDimension
+import common.collateByDimension as cbd
+
+
+cbd.collate(, ['test1.nc','test2.nc'], varname=None, output_filename="out.nc" ):
