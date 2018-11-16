37c8345a704c727cad2ff5729616a4081f7f4f49
Riley Conroy <rpconroy@ucar.edu>
2018-11-16 11:43:53 -0700

    Allowed better access from other programs

--- test/create_test_nc_file.py
+++ test/create_test_nc_file.py
@@ -6,6 +6,9 @@ import numpy as np
 
 
 def get_test_filename():
+    """Returns an unused default test name.
+    Will be of form test[num].nc
+    """
     filename = 'test'
     extension = '.nc'
     i = 1
@@ -13,7 +16,9 @@ def get_test_filename():
         i += 1
     return filename + str(i) + extension
 
-def create_test_file(name=None, num_dims=3, dim_len=10, varname='Test'):
+def create(name=None, num_dims=3, dim_len=10, varname='Test'):
+    """Creates raw test file.
+    """
     if name is None:
         name = get_test_filename()
     nc = Dataset(name, 'w')
@@ -36,6 +41,7 @@ def create_test_file(name=None, num_dims=3, dim_len=10, varname='Test'):
     cur_var = nc.createVariable(varname, float, tuple(dim_names))
     cur_var[:] = np.random.rand(*[dim_len for i in dim_names])
     nc.close()
+    return name
 
 
 
@@ -46,9 +52,18 @@ description = "Creates a test netcdf file."
 parser = argparse.ArgumentParser(prog='create_test_nc_file', description=description)
 parser.add_argument('--filename','-fn', type=str, help="Specify output filename")
 parser.add_argument('--num-dims','-n', type=str, help="Specify number of dimensions")
+parser.add_argument('--dim-length','-dl', type=str, help="Specify length of dimensions")
+parser.add_argument('--varname','-v', type=str, help="Specify primary variable's name")
 
 args = parser.parse_args()
-create_test_file()
+
+name = create(name=args.filename,
+        num_dims=args.num-dims,
+        dim_len=args.dim-length,
+        varname=args.varname)
+
+print(name)
+
 
 
 
--- test/test_collateByDimension.py
+++ test/test_collateByDimension.py
@@ -2,6 +2,7 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) +"/../")
 import common.collateByDimension as cbd
+import create_test_nc_file as create_nc
 
 
 cbd.collate(, ['test1.nc','test2.nc'], varname=None, output_filename="out.nc" ):
