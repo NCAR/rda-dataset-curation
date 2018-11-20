405d1f3c2b881ac59080389c3f2b9ebdfd5de256
Riley Conroy <rpconroy@ucar.edu>
2018-11-20 09:02:35 -0700

    Finished isGrib1

--- common/isGrib1.py
+++ common/isGrib1.py
@@ -11,9 +11,23 @@ def usage():
 
 
 def is_grib1(in_file):
-    pass
+    with open(in_file, 'rb') as f:
+        first8 = f.read()[:8]
+
+    if first8[:4] != b'GRIB':
+        exit(99)
+
+    if first8[-1] == 1:
+        print('True')
+        exit(0)
+    elif first8[-1] == 2:
+        print('False')
+        exit(1)
+
+    exit(99)
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         usage()
+    is_grib1(sys.argv[1])
 
