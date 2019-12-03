37b118ecd7581054c6aa425412bf651c503f9805
Riley Conroy <rpconroy@ucar.edu>
2019-12-03 13:23:38 -0700

    update

---
+++ common/upload_to_zarr/README.md
@@ -0,0 +1,16 @@
+
+# Uploading a dataset to zarr
+
+### Create A config
+```
+usage: ./createDatasetConfig.py [-h] [--json] [--yaml] [--skip_errors]
+                                [--title TITLE]
+                                files [files ...]
+```
+
+
+### Upload Files
+```
+usage: ./upload_zarr.py [-h] [--config CONFIG] [--skip_errors] [--title TITLE]
+                        files [files ...]
+```
--- common/upload_to_zarr/upload_zarr.py
+++ common/upload_to_zarr/upload_zarr.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 import sys
-import zarr
+sys.path.insert(0,'/glade/u/home/rpconroy/.local/lib/python3.6/site-packages/')
 import xarray
+import zarr
 import argparse
+import os
+import json
+import yaml
 
 def get_arguments():
     """Parses arguments and returns arguments.
@@ -15,12 +19,46 @@ def get_arguments():
     parser.add_argument('files', nargs="+", help="Files or directory to scan.")
     return parser.parse_args()
 
+def get_files_from_dir(directory):
+    """Given directory, return a list of files.
+    """
+    files = []
+    for path in os.walk(directory):
+        dirpath = path[0]
+        dirnames = path[1]
+        filenames = path[2]
+        if len(filenames) == 0:
+            continue
+        for f in filenames:
+            files.append(dirpath+'/'+f)
+    return files
+
+def get_variable_config(config, var):
+    pass
+
 
 if __name__ == "__main__":
+
     args = get_arguments()
+    title = "test"
+    if args.title is not None:
+        title = args.title
+    filenames = args.files
+    if args.config is not None:
+        ext = args.config.split('.')[-1]
+        if ext == 'json':
+            config = json.load(open(args.config))
+        elif ext == 'yaml':
+            config = yaml.load(open(args.config))
+        else:
+            raise ValueError("config format not recognized")
+
+    for filename in filenames:
+        if os.path.isdir(filename):
+            filenames.extend(get_files_from_dir(filename))
+            continue
+        ds = xarray.open_dataset(filename)
+        ds.to_zarr(title+'.zarr', mode='a', append_dim='time')
 
 
-    ds = xarray.open_dataset(filename)
-    #mmk.chunk({})
-    ds.to_zarr(filename+'.zarr')
 
